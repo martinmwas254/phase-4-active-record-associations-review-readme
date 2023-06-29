5908303ef3596e0bf0f841546f5b4b971daa39f7
Martin <Martin@Martin>
2023-06-29 13:14:53 +0300

    Completed assignment

--- db/migrate/20210506144424_create_authors.rb
+++ db/migrate/20210506144424_create_authors.rb
@@ -2,7 +2,7 @@ class CreateAuthors < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
       t.string :name
-
+      t.string :username
       t.timestamps
     end
   end
